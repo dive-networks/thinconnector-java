e255e368dce55062c50837eb81db2f12dbf7acc9
noonan <andrew@noonan.cc>
2014-03-19 14:34:55 -0600

    rename

--- src/main/java/com/gnip/stream/GnipAPI.java
+++ src/main/java/com/gnip/stream/GnipAPI.java
@@ -7,16 +7,16 @@ import com.gnip.utilities.TaskManager;
 import com.google.inject.Inject;
 import org.apache.log4j.Logger;
 
-public class GnipStream {
+public class GnipAPI {
     private final Logger logger = Logger.getLogger(getClass());
     private final Environment environment;
     private final StreamProcessor processor;
     private TaskManager taskManager;
 
     @Inject
-    public GnipStream(Environment environment,
-                      StreamProcessor processor,
-                      TaskManager taskManager) {
+    public GnipAPI(Environment environment,
+                   StreamProcessor processor,
+                   TaskManager taskManager) {
         this.environment = environment;
         this.processor = processor;
         this.taskManager = taskManager;
